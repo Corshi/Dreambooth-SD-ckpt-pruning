3cd531acc198142d51eeefc81c36cf31b1ab2a22
Corshi <wedrix@gmail.com>
2022-12-03 21:47:41 +0000

    Update prune-ckpt.py

--- prune-ckpt.py
+++ prune-ckpt.py
@@ -1,3 +1,9 @@
+#usage: prune-ckpt.py [-h] [--ckpt CKPT] [--half] [--emaonly]
+#This script will crated `archive` file in the directory of this script. You will ned to rename it manualy to .ckpt file.
+#This is to avoid the error of PyTorch that incorectly names the directories inside the .ckpt file.
+#Also with aditional commands you can now set what kind of data you want to prune next to optimizers.
+#--half will crate flout16 version
+#--emaonly will erase MA data. Leavin only EMA.
 import os
 import torch
 import argparse
@@ -5,11 +11,13 @@ import glob
 
 
 parser = argparse.ArgumentParser(description='Pruning')
-parser.add_argument('--ckpt', type=str, default=None, help='path to model ckpt')
+parser.add_argument('--ckpt', type=str, default=None, help='Path to model ckpt')
+parser.add_argument("--half", action="store_true", help="Save weights in half precision.")
+parser.add_argument('--emaonly', action="store_true", help='Keep only ema data')
 args = parser.parse_args()
 ckpt = args.ckpt
 
-def prune_it(p, keep_only_ema=False):
+def prune_it(p):
     print(f"prunin' in path: {p}")
     size_initial = os.path.getsize(p)
     nsd = dict()
@@ -22,17 +30,17 @@ def prune_it(p, keep_only_ema=False):
         print(f"removing optimizer states for path {p}")
     if "global_step" in sd:
         print(f"This is global step {sd['global_step']}.")
-    if keep_only_ema:
+    if args.emaonly:
         sd = nsd["state_dict"].copy()
         # infer ema keys
-        ema_keys = {k: "model_ema." + k[6:].replace(".", ".") for k in sd.keys() if k.startswith("model.")}
+        ema_keys = {k: "model_ema." + k[6:].replace(".", "") for k in sd.keys() if k.startswith("model.")}
         new_sd = dict()
 
         for k in sd:
             if k in ema_keys:
-                new_sd[k] = sd[ema_keys[k]].half()
-            elif not k.startswith("model_ema.") or k in ["model_ema.num_updates", "model_ema.decay"]:
-                new_sd[k] = sd[k].half()
+                new_sd[k] = sd[ema_keys[k]] if not args.half else sd[ema_keys[k]].half()
+            elif not k.startswith("model_ema") or k in ["model_ema.num_updates", "model_ema.decay"]:
+                new_sd[k] = sd[k] if not args.half else sd[k].half()
 
         assert len(new_sd) == len(sd) - len(ema_keys)
         nsd["state_dict"] = new_sd
@@ -40,16 +48,16 @@ def prune_it(p, keep_only_ema=False):
         sd = nsd['state_dict'].copy()
         new_sd = dict()
         for k in sd:
-            new_sd[k] = sd[k].half()
+            new_sd[k] = sd[k] if not args.half else sd[k].half()
         nsd['state_dict'] = new_sd
 
-    fn = f"{os.path.splitext(p)[0]}-pruned.ckpt" if not keep_only_ema else f"{os.path.splitext(p)[0]}-ema-pruned.ckpt"
+    fn = f"archive"
     print(f"saving pruned checkpoint at: {fn}")
     torch.save(nsd, fn)
     newsize = os.path.getsize(fn)
     MSG = f"New ckpt size: {newsize*1e-9:.2f} GB. " + \
           f"Saved {(size_initial - newsize)*1e-9:.2f} GB by removing optimizer states"
-    if keep_only_ema:
+    if args.emaonly:
         MSG += " and non-EMA weights"
     print(MSG)
 
